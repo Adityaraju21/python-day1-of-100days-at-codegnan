a725fa059e3b805ffa7bd8b528e8b5184ed51483
Adityaraju21 <adithyaraj7396@gmail.com>
2026-01-08 22:26:30 +0530

    Add files via upload

---
+++ day2 -datatype_vrabiles_fuctions.py
@@ -0,0 +1,17 @@
+# dataype varibles and 10 function\
+name = "Aditya_Raju" # sring type
+age = 32 # integer
+weight = 100.264 # float datatype
+List1 = [1,2,3,4,5,'hi', 2.4, (10,20)] #list of items
+tuple1 = (10,20,30,'welcome', 'hello') # tuple of items
+cares = {'A',2,3,4,5,6, 'J', 'E', 'R',1,2,3}# pack of card in set datatype
+student_marks = { 'maths' :80,'telugu' :70, 'english':90} # dictnory datatype
+
+print("my name is:", name)
+print("my age is", age)
+print("my weight is:", weight)
+print("list of itens:", list)
+print("tupleof of items:", tuple1)
+print("pack of cards vlaues:", cares)
+print("student subject wise marks:", student_marks)
+
